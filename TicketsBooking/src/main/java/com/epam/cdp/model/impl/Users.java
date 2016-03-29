7c6e1f8ef008b772812b47c4a90ee047cb3051f8
Denys Uzhvii <denys_uzhvii@epam.com>
2016-03-29 11:19:43 +0300

    Task2

---
+++ TicketsBooking/src/main/java/com/epam/cdp/model/impl/Users.java
@@ -0,0 +1,15 @@
+package com.epam.cdp.model.impl;
+
+import java.util.List;
+
+public class Users {
+    private List<UserEntity> users;
+
+    public List<UserEntity> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<UserEntity> users) {
+        this.users = users;
+    }
+}
